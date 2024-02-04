e26ef6c3f9775df0c988ff53882573ea6dfb5c61
am999 <am9999072080@gmail.com>
2024-02-04 23:22:45 +0300

    commit_1

---
+++ src/main/java/com/consulting/average/AverageApplication.java
@@ -0,0 +1,12 @@
+package com.consulting.average;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AverageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AverageApplication.class, args);
+    }
+}
