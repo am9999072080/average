d7fa8b9fabd513372d95d25840114498554a593b
am999 <am9999072080@gmail.com>
2024-02-12 02:19:08 +0300

    method_2

---
+++ src/main/java/com/consulting/average/controller/AverageController.java
@@ -0,0 +1,35 @@
+package com.consulting.average.controller;
+
+import com.consulting.average.service.AverageValueImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+
+@Tag(name = "CALCULATE THE WEIGHTED AVERAGE", description = "AverageController")
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/value")
+
+public class AverageController {
+    private final AverageValueImpl service;
+
+
+    @GetMapping("Weighted Average")
+    @Operation(summary = "weightForEachDataPoint/valueOfEachDataPoint")
+    public double calculatesTheWeightedAverage(@RequestParam ArrayList<Double> w,
+                                               @RequestParam ArrayList<Double> v) {
+
+        return service.calculateWeightedAverage(w, v);
+    }
+
+    @ExceptionHandler({RuntimeException.class})
+    public String errorNotification(RuntimeException e) {
+        String s = "INPUT ERROR. Enter numbers only! '[0-9]+.[0-9]'";
+        return s;
+    }
+}
+
